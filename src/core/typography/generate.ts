92e927dbd13a1cb4638d6aad359108933eb12d16
Gleb Andreev <andreevgs.dev@yahoo.com>
2026-08-18 03:13:18 +0200

    feat: support for static fonts

--- src/core/typography/generate.ts
+++ src/core/typography/generate.ts
@@ -18,12 +18,28 @@ export const generateMateriaTypography = (
       variant.startsWith("display") || variant.startsWith("headline");
 
     const activeTypeface = isBrand ? brand : plain;
+    const targetWeight = activeTypeface.fontWeight ?? token.fontWeight;
+
+    let resolvedFontFamily: string;
+    let resolvedFontWeight = targetWeight;
+
+    if (
+      activeTypeface.fontFamily &&
+      typeof activeTypeface.fontFamily === "object"
+    ) {
+      resolvedFontFamily =
+        targetWeight === "500"
+          ? activeTypeface.fontFamily.fontFamilyMedium
+          : activeTypeface.fontFamily.fontFamilyRegular;
+    } else {
+      resolvedFontFamily = activeTypeface.fontFamily;
+    }
 
     typography[variant] = {
       ...(fixVerticalRhythm ? defaultCommonStyles : {}),
       ...token,
-      fontFamily: activeTypeface.fontFamily,
-      fontWeight: activeTypeface.fontWeight ?? token.fontWeight,
+      fontFamily: resolvedFontFamily,
+      fontWeight: resolvedFontWeight,
     };
   });
 
