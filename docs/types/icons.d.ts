9d8c274643120139b395a28d09256ee276afdbcc
Gleb Andreev <andreevgs.dev@yahoo.com>
2026-08-26 03:02:56 +0200

    docs: implement icons support, add necessary icons

--- docs/types/icons.d.ts
+++ docs/types/icons.d.ts
@@ -4,5 +4,8 @@ import { MateriaIcon } from "react-native-materia/types";
 declare module "react-native-materia/types" {
   export interface MateriaIconography {
     "github": MateriaIcon;
+    "layers-rounded": MateriaIcon;
+    "wifi-rounded": MateriaIcon;
+    "bluetooth-rounded": MateriaIcon;
   }
 }
